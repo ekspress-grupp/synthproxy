56f98ee3abcaaa86e2d17cd87fa90baa1611be85
Siim Tiilen <siim.tiilen@ekspressdigital.eu>
2018-09-20 15:58:25 +0300

    add body parser to handle posts data

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 const app: express.Express = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/health_check', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/synth/v1/health_check', (req, res) => {
   res.send(`ok:${+new Date()}[${require('os').hostname()}]`);
 });
 

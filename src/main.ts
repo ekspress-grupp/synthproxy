b68402e970f19e257a523333e33d0c87b70ca1ac
Siim Tiilen <siim.tiilen@ekspressdigital.eu>
2018-09-20 15:58:43 +0300

    add sync entry point

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,30 @@
-import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as express from 'express';
+
+import sync from './sync';
 
 const app: express.Express = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/synth/v1/health_check', (req, res) => {
   res.send(`ok:${+new Date()}[${require('os').hostname()}]`);
 });
 
-app.get('/', (req, res) => {
-  res.send(`hello world!`);
+// curl -X POST "http://localhost:3000/synth/v1/synth" --data "text=test kala"
+app.post('/synth/v1/synth', async (req, res) => {
+  const { text } = req.body;
+  if (!text) {
+    return res.status(400).json({ error: 'missing text' });
+  }
+  const url = await sync(text);
+  res.json({
+    url,
+    // FIXME: remove - for testing!
+    text,
+  });
 });
 
 app.listen(port, () => {
---
+++ src/sync.ts
@@ -0,0 +1,5 @@
+export default async (text: string): Promise<string> => {
+  console.log('sync-text', text);
+  // TODO: make cli call
+  return 'https://exampl.com/test.mp3';
+};
